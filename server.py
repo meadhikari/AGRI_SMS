d48b42cdbec89e69a73b6ea04acf3085ab74d23d
Bikram Adhikari <salik.adhikari@gmail.com>
2015-07-29 23:21:25 +0545

    keyword removed from the decoding text

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = web.application(urls, globals())
 class hello:        
     def GET(self,name):
         user_data = web.input()
-        return correct(user_data.text)
+        return correct(user_data.text.replace('sapiAGRI ',''))
 
 if __name__ == "__main__":
     app.run()
